33005cbac6cf68d17331e78e866f2d3004e78179
Capt.Jeffrey <penga12@naver.com>
2024-09-13 01:39:19 +0900

    Delete Source directory

--- Source/Survivorlise/Widget/SLLoadingWidget.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Widget/SLLoadingWidget.h"
-
-void USLLoadingWidget::NativeConstruct()
-{
-	Super::NativeConstruct();
-}
